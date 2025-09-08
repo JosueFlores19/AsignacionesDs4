0cc626ab2105a1935277ec2f51643e52c8465fa2
JosueFlores19 <josueedsau.flores@utp.ac.pa>
2025-09-08 10:28:23 -0500

    Se subio las asignaciones

---
+++ RepasoDS4C#.2/RepasoDS4C#.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var radio = 0d;
+var resultado = 0d;
+
+// declaraciones de constantes, siempre empezar con Mayusculas las constantes
+const double Pi = 3.14;
+
+Console.WriteLine("Bienvenido, calculemos circulos");
+// Pi * radio * radio
+
+Console.WriteLine("Ingrese el valor del radio");
+radio = Convert.ToInt32(Console.ReadLine());
+
+resultado = Math.PI * radio *radio;
+// resultado = Pi * radio * radio;
+// usando la constante
+
+Console.WriteLine("El area del circulo es de: " + resultado); 
---
+++ RepasoDS4C#/RepasoDS4C#/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//Programa que calcula el área de un rectángulo
+//Si se usa var 
+//var ladoA
+//var ladoB
+// no recomendable si se sabe que tipos de datos se usara
+// float ladoA
+// float ladoB
+// float Rectangulo
+
+
+// Declaracion de variables 
+int ladoA;
+int ladoB;
+int Rectangulo;
+
+Console.WriteLine("Calcula el área de un rectángulo");
+Console.WriteLine("Ingrese el lado A:");
+ladoA = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Ingrese el lado B: ");
+ladoB = Convert.ToInt32(Console.ReadLine());
+
+Rectangulo = ladoA * ladoB;
+Console.WriteLine("El lado A es de:" + ladoA + " El lado B es de:" + ladoB + " El área del rectángulo es: " + Rectangulo);
