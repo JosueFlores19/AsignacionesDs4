36fc4a25d780f17df1c5967d97f34839eeacf909
JosueFlores19 <josueedsau.flores@utp.ac.pa>
2025-09-09 21:26:20 -0500

    Videos 11, 12, 13

---
+++ DS4C#.3/DS4C#.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// cambiar los valores entre true y flase para probar los operadores logicos
+bool valor1 = true;
+bool valor2 = true;
+bool valor3 = true;
+
+/* 
+&& (and): Devuelve verdadero si ambos operandos son verdaderos.
+|| (or): Devuelve verdadero si al menos uno de los operandos es verdadero.
+! (not): Invierte el valor booleano del operando.
+^ (xor): Devuelve verdadero si sólo uno de los operandos es verdadero.
+*/
+
+bool resultAnd = valor1 && valor2 && valor3;
+Console.WriteLine(resultAnd);
+
+bool resultOr = valor1 || valor2 || valor3;
+Console.WriteLine(resultOr);
+
+bool resultAndOr = (valor1 && valor2) || valor3;
+Console.WriteLine(resultAndOr);
+
+bool resultadoNot = !valor1;
+Console.WriteLine(resultadoNot);
+
+bool resultadoXor = valor1 ^ valor2; //XOR = not or (es decir lo opuesto a la or)
+Console.WriteLine(resultadoXor);
+
+// bool resultadoXor = (valor1 ^ valor2) ^ valor3; 
+//XOR ^ XOR es decir primero saca el binario T/F de la primera Xor y luego lo compara con la segunda
+// Console.WriteLine(resultadoXor);
---
+++ DS4C#.5/DS4C#.5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Josue Flores ID!");
+int altura = 182;
+int edad = 20;
+string nombre = "Josue Flores";
+string informacion = "Nació en Santigo, es estudiante programacion, musica, dibujo y lenguas";
+var hobby = "Artista";
+
+
+string tarjetaDeIdentificacion = $"La información de {nombre} es la siguiente \nSu edad es {edad}\n" +
+    $"su altura es de {altura} cms\nInformación relevante {informacion} \nademás, su hobby es ser: {hobby} ";
+
+Console.WriteLine(tarjetaDeIdentificacion);
